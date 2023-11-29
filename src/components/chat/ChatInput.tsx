34eec26b0d74b1462d464e57e07f47c0a8c64aea
Beymer Bustamante <bustamantef@unprg.edu.pe>
2023-11-29 16:16:19 -0500

    Loading states

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,9 @@
+interface ChatInputProps {
+  isDisabled?: boolean;
+}
+
+function ChatInput({ isDisabled }: ChatInputProps) {
+  return <div>ChatInput</div>;
+}
+
+export default ChatInput;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -30,6 +30,7 @@ export const appRouter = router({
 
     return { success: true };
   }),
+
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
     const { userId } = ctx;
     // const id = user.id!;
@@ -40,6 +41,22 @@ export const appRouter = router({
       },
     });
   }),
+
+  getFileUploadStatus: privateProcedure
+    .input(z.object({ fileId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const file = await db.file.findFirst({
+        where: {
+          id: input.fileId,
+          userId: ctx.userId,
+        },
+      });
+
+      if (!file) return { status: "PENDING" as const };
+
+      return { status: file.uploadStatus };
+    }),
+
   getFile: privateProcedure
     .input(z.object({ key: z.string() }))
     .mutation(async ({ ctx, input }) => {
@@ -55,6 +72,7 @@ export const appRouter = router({
 
       return file;
     }),
+
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
